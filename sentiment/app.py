843cd54aff7e10117679f74722759762c23809ac
zjm <jz1g17@ecs.soton.ac.uk>
2017-11-14 07:25:03 +0000

    sentiment
    back and front file ready
    run app.py

--- sentiment/app.py
+++ sentiment/app.py
@@ -1,4 +1,170 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request, jsonify
+import json
+from watson_developer_cloud import NaturalLanguageUnderstandingV1
+import watson_developer_cloud.natural_language_understanding.features.v1 \
+  as Features
+
+natural_language_understanding = NaturalLanguageUnderstandingV1(
+    username="3a1f4a06-4cbd-4193-81e7-a698805aef3e",
+    password="BGIu8SBYwgB3",
+    version="2017-02-27"
+    )
+
+def GetMiddleStr(target,frontStr,backStr):
+  frontIndex = target.index(frontStr)
+  if frontIndex>=0:
+    frontIndex += len(frontStr)
+  backIndex = target.index(backStr)
+  return target[frontIndex:backIndex]
+
+def GetLineStr(output):
+    alllines = output.split('\n')
+    lines=[]
+    for oneline in alllines:
+        lines.append(oneline)
+    return lines
+
+def GetKeyword(num,lines):
+    key = []
+    if num != 5:
+        for i in range(1,num+1):
+            a = GetMiddleStr(lines[9 + (i - 1) * 15], '"text": "', '", ')
+            key.append(a)
+        for k in range(num+1,5+1):
+            b = "No data"
+            key.append(b)
+    if num == 5:
+        for j in range(1,num+1):
+            a = GetMiddleStr(lines[9 + (j - 1) * 15], '"text": "', '", ')
+            key.append(a)
+    return key
+
+def GetAnger(num,lines):
+    anger = []
+    if num != 5:
+        for i in range(1,num+1):
+            a = GetMiddleStr(lines[11 + (i - 1) * 15], '"anger": ', ', ')
+            anger.append(a)
+        for k in range(num+1,5+1):
+            b = "No data"
+            anger.append(b)
+    if num == 5:
+        for j in range(1,num+1):
+            a = GetMiddleStr(lines[11 + (j - 1) * 15], '"anger": ', ', ')
+            anger.append(a)
+    return anger
+
+def GetJoy(num,lines):
+    joy = []
+    if num != 5:
+        for i in range(1,num+1):
+            a = GetMiddleStr(lines[12 + (i - 1) * 15], '"joy": ', ', ')
+            joy.append(a)
+        for k in range(num+1,5+1):
+            b = "No data"
+            joy.append(b)
+    if num == 5:
+        for j in range(1,num+1):
+            a = GetMiddleStr(lines[12 + (j - 1) * 15], '"joy": ', ', ')
+            joy.append(a)
+    return joy
+
+def GetSadness(num,lines):
+    sadness = []
+    if num != 5:
+        for i in range(1,num+1):
+            a = GetMiddleStr(lines[13 + (i - 1) * 15], '"sadness": ', ', ')
+            sadness.append(a)
+        for k in range(num+1,5+1):
+            b = "No data"
+            sadness.append(b)
+    if num == 5:
+        for j in range(1,num+1):
+            a = GetMiddleStr(lines[13 + (j - 1) * 15], '"sadness": ', ', ')
+            sadness.append(a)
+    return sadness
+
+def GetFear(num,lines):
+    fear = []
+    if num != 5:
+        for i in range(1,num+1):
+            a = GetMiddleStr(lines[14 + (i - 1) * 15], '"fear": ', ', ')
+            fear.append(a)
+        for k in range(num+1,5+1):
+            b = "No data"
+            fear.append(b)
+    if num == 5:
+        for j in range(1,num+1):
+            a = GetMiddleStr(lines[14 + (j - 1) * 15], '"fear": ', ', ')
+            fear.append(a)
+    return fear
+
+def GetDisgust(num,lines):
+    disgust = []
+    if num != 5:
+        for i in range(1,num+1):
+            c = lines[15 + (i - 1) * 15] + lines[16 + (i - 1) * 15]
+            a = GetMiddleStr(c, '"disgust": ', '      }, ')
+            disgust.append(a)
+        for k in range(num+1,5+1):
+            b = "No data"
+            disgust.append(b)
+    if num == 5:
+        for j in range(1,num+1):
+            c = lines[15 + (j - 1) * 15] + lines[16 + (j - 1) * 15]
+            a = GetMiddleStr(c, '"disgust": ', '      }, ')
+            disgust.append(a)
+    return disgust
+
+def GetEmotion(num,lines):
+     emotion = []
+     anger = GetMiddleStr(lines[-10], '"anger": ', ', ')
+     emotion.append(anger)
+     joy = GetMiddleStr(lines[-9], '"joy": ', ', ')
+     emotion.append(joy)
+     sadness = GetMiddleStr(lines[-8], '"sadness": ', ', ')
+     emotion.append(sadness)
+     fear = GetMiddleStr(lines[-7], '"fear": ', ', ')
+     emotion.append(fear)
+     c = lines[-6] + lines[-5]
+     disgust = GetMiddleStr(c, '"disgust": ', '      }')
+     emotion.append(disgust)
+     return emotion
+
+
+def sentiment_output(shuru):
+    response = natural_language_understanding.analyze(
+        text = shuru,
+        features=[
+            Features.Emotion(),
+            Features.Keywords(
+                emotion=True,
+                sentiment=True,
+                limit=5
+            )
+        ]
+    )
+
+    output = json.dumps(response,indent=2)
+    lines = GetLineStr(output)
+
+    rows = len(lines)
+    num = (rows-21)/14
+
+    key = GetKeyword(num,lines)
+    anger = GetAnger(num,lines)
+    joy = GetJoy(num,lines)
+    sadness = GetFear(num,lines)
+    fear = GetFear(num,lines)
+    disgust = GetDisgust(num,lines)
+    emotion = GetEmotion(num,lines)
+
+    sum1 = key + anger + joy + sadness + fear + disgust + emotion
+
+    return (sum1)
+
+
+
 
 app = Flask(__name__)
 
@@ -6,15 +172,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/')
-def data():
- # here we want to get the value of user (i.e.?user=some-value)
- info = request.data()
- return info
+
+@app.route('/api/sentiment', methods=['POST'])
+
+def say_name():
+    json = request.get_json()
+    output = json['sen_test']
+    output1 = sentiment_output(output)
+
+    return jsonify(textinformation=output1)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-x=data()
-print(x)
--- sentiment/verson.1/team_E_sentiment.py
+++ sentiment/verson.1/team_E_sentiment.py
@@ -1,9 +1,13 @@
-from xlrd import open_workbook
-from xlutils.copy import copy
 import json
 from watson_developer_cloud import NaturalLanguageUnderstandingV1
 import watson_developer_cloud.natural_language_understanding.features.v1 \
   as Features
+
+natural_language_understanding = NaturalLanguageUnderstandingV1(
+ username="3a1f4a06-4cbd-4193-81e7-a698805aef3e",
+ password="BGIu8SBYwgB3",
+ version="2017-02-27"
+)
 
 def GetMiddleStr(target,frontStr,backStr):
   frontIndex = target.index(frontStr)
@@ -19,7 +23,7 @@ def GetLineStr(output):
         lines.append(oneline)
     return lines
 
-def GetKeyword(num):
+def GetKeyword(num,lines):
     key = []
     if num != 5:
         for i in range(1,num+1):
@@ -34,7 +38,7 @@ def GetKeyword(num):
             key.append(a)
     return key
 
-def GetAnger(num):
+def GetAnger(num,lines):
     anger = []
     if num != 5:
         for i in range(1,num+1):
@@ -49,7 +53,7 @@ def GetAnger(num):
             anger.append(a)
     return anger
 
-def GetJoy(num):
+def GetJoy(num,lines):
     joy = []
     if num != 5:
         for i in range(1,num+1):
@@ -64,7 +68,7 @@ def GetJoy(num):
             joy.append(a)
     return joy
 
-def GetSadness(num):
+def GetSadness(num,lines):
     sadness = []
     if num != 5:
         for i in range(1,num+1):
@@ -79,7 +83,7 @@ def GetSadness(num):
             sadness.append(a)
     return sadness
 
-def GetFear(num):
+def GetFear(num,lines):
     fear = []
     if num != 5:
         for i in range(1,num+1):
@@ -94,7 +98,7 @@ def GetFear(num):
             fear.append(a)
     return fear
 
-def GetDisgust(num):
+def GetDisgust(num,lines):
     disgust = []
     if num != 5:
         for i in range(1,num+1):
@@ -111,7 +115,7 @@ def GetDisgust(num):
             disgust.append(a)
     return disgust
 
-def GetEmotion(num):
+def GetEmotion(num,lines):
      emotion = []
      anger = GetMiddleStr(lines[-10], '"anger": ', ', ')
      emotion.append(anger)
@@ -139,51 +143,40 @@ def input():
     strings = raw_input("Please input the target text:\n")
     return strings
 
-natural_language_understanding = NaturalLanguageUnderstandingV1(
-  username="3a1f4a06-4cbd-4193-81e7-a698805aef3e",
-  password="BGIu8SBYwgB3",
-  version="2017-02-27"
-)
 
-response = natural_language_understanding.analyze(
-  text=input(),
-  features=[
-      Features.Emotion(),
-      Features.Keywords(
-      emotion=True,
-      sentiment=True,
-      limit=5
+
+
+def sentiment_output(shuru):
+    response = natural_language_understanding.analyze(
+        text = shuru,
+        features=[
+            Features.Emotion(),
+            Features.Keywords(
+                emotion=True,
+                sentiment=True,
+                limit=5
+            )
+        ]
     )
-  ]
-)
 
-output=json.dumps(response, indent=2)
-lines = GetLineStr(output)
+    output = json.dumps(response,indent=2)
+    lines = GetLineStr(output)
 
-rows = len(lines)
-num = (rows-21)/14
+    rows = len(lines)
+    num = (rows-21)/14
 
-key = GetKeyword(num)
-anger = GetAnger(num)
-joy = GetJoy(num)
-sadness = GetSadness(num)
-fear = GetFear(num)
-disgust = GetDisgust(num)
-emotion = GetEmotion(num)
+    key = GetKeyword(num,lines)
+    anger = GetAnger(num,lines)
+    joy = GetJoy(num,lines)
+    sadness = GetFear(num,lines)
+    fear = GetFear(num,lines)
+    disgust = GetDisgust(num,lines)
+    emotion = GetEmotion(num,lines)
 
+    sum1 = key + anger + joy + sadness + fear + disgust + emotion
 
-print(key)
-print(anger)
-print(joy)
-print(sadness)
-print(fear)
-print(disgust)
-print(emotion)
+    return (sum1)
 
-outputToText(key)
-outputToText(anger)
-outputToText(joy)
-outputToText(sadness)
-outputToText(fear)
-outputToText(disgust)
-outputToText(emotion)
+aa = "I love tom and bannana"
+bb = sentiment_output(aa)
+print(bb)
