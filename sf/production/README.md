e0cb45915d8b5f98198c60a596f5cfe2fd8afdc1
promer94 <yixuanxu94@gmail.com>
2017-12-06 17:49:30 +0000

    Update

--- sf/production/README.md
+++ sf/production/README.md
@@ -12,10 +12,7 @@ This will download and install the dependencies listed in package.json.
 8) Using the following command:
    python main.py
 9) Visit http://localhost:5000/
-
-
 ## Set up
-
 * 1. Create and enter an isolated Python environment using virtualenv:  
    virtualenv env 
    source env/bin/activate
@@ -24,8 +21,7 @@ At the end of the tutorial, you can exit your virtualenv by typing deactivate
 * 2. Install dependencies using pip:
       pip install -t lib -r requirements.txt
 The -t lib flag copies the libraries into a lib folder, which is uploaded to App Engine during deployment. See Installing a third-party library for more information about the vendoring process.The -r requirements.txt flag tells pip to install everything from a requirements.txt file.
-
-   ## Test the application
+## Test the application
 Test the application using the local development server (dev_appserver.py), which is included with the SDK.
 * 1. From within the root directory where the app's app.yaml configuration file is located, start the local development server with the following command:
 
--- sf/production/main.py
+++ sf/production/main.py
@@ -1,21 +1,20 @@
-# from flask import Flask, render_template, jsonify, request
-# import flask_cors
-# import google.auth.transport.requests
-# import google.oauth2.id_token
-# import requests_toolbelt.adapters.appengine
-
 from flask import Flask, flash, render_template, jsonify, request, redirect, url_for, session
 from flask_oauth import OAuth
 from functools import wraps
+import simplejson as json
+from pymongo import MongoClient
 
 # You must configure these 3 values from Google APIs console
 # https://code.google.com/apis/console
-GOOGLE_CLIENT_ID = '391803100925-vionpfd5f89onvota5pbqq8rv5u0jt6n.apps.googleusercontent.com'
-GOOGLE_CLIENT_SECRET = '6G8dclxrryMHI-_ol6Mtymjp'
+GOOGLE_CLIENT_ID = '119855237642-p3ckimhcgmb2ljnigcegvrh1v43eb1ba.apps.googleusercontent.com'
+GOOGLE_CLIENT_SECRET = 'eVGXh0tufuuqgY5YK0zz0Sck'
 # one of the Redirect URIs from Google APIs console
 REDIRECT_URI = '/oauth2callback'
-
 SECRET_KEY = 'development key'
+
+client = MongoClient('mongodb://user:Sanbao941104@socialfamily-shard-00-00-03pay.mongodb.net:27017,socialfamily-shard-00-01-03pay.mongodb.net:27017,socialfamily-shard-00-02-03pay.mongodb.net:27017/test?ssl=true&replicaSet=SocialFamily-shard-0&authSource=admin')
+db = client.SocialFamily
+collection = db.user
 
 
 app = Flask(__name__)
@@ -36,6 +35,7 @@ google = oauth.remote_app('google',
                           consumer_key=GOOGLE_CLIENT_ID,
                           consumer_secret=GOOGLE_CLIENT_SECRET)
 
+
 #@app.before_request
 # def before_request():
 #    if request.url.startswith('http://'):
@@ -46,6 +46,72 @@ google = oauth.remote_app('google',
 @app.route('/')
 #@app.before_request
 def index():
+    return loginWithgoogle()
+
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/login')
+def login():
+    callback = url_for('authorized', _external=True)
+    return google.authorize(callback=callback)
+
+
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, Please login', 'danger')
+            return redirect(url_for('home'))
+    return wrap
+
+
+@app.route('/register')
+@is_logged_in
+def register():
+    flash('Hello New Friend!', 'success')
+    return render_template('dashborad.html')
+
+
+@app.route('/dashborad')
+@is_logged_in
+def dashborad():
+    flash('You are now logged in', 'success')
+    return render_template('dashborad.html')
+
+
+@app.route('/logout')
+@is_logged_in
+def logout():
+    session.clear()
+    flash('You are now logged out', 'success')
+    return redirect(url_for('index'))
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route(REDIRECT_URI)
+@google.authorized_handler
+def authorized(resp):
+    access_token = resp['access_token']
+    session['access_token'] = access_token, ''
+    return redirect(url_for('index'))
+
+
+@google.tokengetter
+def get_access_token():
+    return session.get('access_token')
+
+
+def loginWithgoogle():
     access_token = session.get('access_token')
     if access_token is None:
         return redirect(url_for('home'))
@@ -66,89 +132,25 @@ def index():
         return res.read()
 
     session['logged_in'] = True
-    session['user_info'] = res.read()
-    return redirect(url_for('dashboard'))
+    user_dict = json.loads(res.read().decode("utf-8"))
 
+    session['user_id'] = user_dict['id']
+    session['user_email'] = user_dict['email']
+    session['user_name'] = user_dict['name']
+    #session['user_given_name'] = user_dict['given_name']
+    #session['user_family_name'] = user_dict['family_name']
+    session['user_picture'] = user_dict['picture']
 
-@app.route('/home')
-def home():
-    return render_template('home.html')
-
-
-@app.route('/login')
-def login():
-    callback = url_for('authorized', _external=True)
-    return google.authorize(callback=callback)
-
-@app.route('/main')
-def main():
-    return render_template('main.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-
-def is_logged_in(f):
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return f(*args, **kwargs)
-        else:
-            flash('Unauthorized, Please login', 'danger')
-            return redirect(url_for('home'))
-    return wrap
-
-
-@app.route('/dashboard')
-@is_logged_in
-def dashboard():
-    flash('You are now logged in', 'success')
-    return render_template('dashboard.html')
-
-
-@app.route('/logout')
-@is_logged_in
-def logout():
-    session.clear()
-    flash('You are now logged out', 'success')
-    return redirect(url_for('index'))
-
-
-@app.route(REDIRECT_URI)
-@google.authorized_handler
-def authorized(resp):
-    access_token = resp['access_token']
-    session['access_token'] = access_token, ''
-    return redirect(url_for('index'))
-
-
-@google.tokengetter
-def get_access_token():
-    return session.get('access_token')
+    new_user = {'googleId': user_dict['id'],
+                'email': user_dict['email'],
+                'picture_link': user_dict['picture']
+                }
+    if(collection.find({'googleId': user_dict['id']}).count() == 0):
+        collection.insert_one(new_user)
+        return redirect(url_for('register'))
+    else:
+        return redirect(url_for('dashborad'))
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-# app = Flask(__name__)
-# flask_cors.CORS(app)
-
-
-# @app.route('/')
-# def index():
-#     return render_template('home.html')
-
-# @app.route('/main')
-# def login():
-#     return render_template('main.html')
-
-# @app.route('/about')
-# def about():
-#     return render_template('about.html')
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
